bda8fc67c0e9889054c305266d0dcefb37195737
Boris Nagaev <bnagaev@gmail.com>
2017-05-20 15:56:47 +0200

    add module kvhttp

---
+++ zipkv/kvhttp/kvhttp.go
@@ -0,0 +1,66 @@
+package kvhttp
+
+import (
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/starius/invisiblefs/zipkv"
+)
+
+type Handler struct {
+	kv       zipkv.KV
+	maxValue int64
+}
+
+func New(kv zipkv.KV, maxValue int) (*Handler, error) {
+	return &Handler{
+		kv:       kv,
+		maxValue: int64(maxValue),
+	}, nil
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path
+	if r.Method == "GET" {
+		value, err := h.kv.Get(key)
+		if err != nil {
+			log.Printf("Get(%q): %s", key, err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		if _, err := w.Write(value); err != nil {
+			log.Printf("Write: %s", err)
+			return
+		}
+	} else if r.Method == "PUT" {
+		if r.ContentLength > h.maxValue {
+			log.Printf("%d > %d", r.ContentLength, h.maxValue)
+			w.WriteHeader(http.StatusRequestEntityTooLarge)
+			return
+		}
+		value := make([]byte, r.ContentLength)
+		if _, err := io.ReadFull(r.Body, value); err != nil {
+			log.Printf("io.ReadFull: %s", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		if err := h.kv.Put(key, value); err != nil {
+			log.Printf("Put(%q): %s", key, err)
+			w.WriteHeader(http.StatusBadGateway)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	} else if r.Method == "DELETE" {
+		if err := h.kv.Delete(key); err != nil {
+			log.Printf("Delete(%q): %s", key, err)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+	} else {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+}
