8ee906f2cc403f31774ae09f6988f3a850ea8247
Boris Nagaev <bnagaev@gmail.com>
2017-08-08 23:29:05 +0100

    sparsetool: close files and unmount fuse on Ctrl+C

--- sparse/sparsetool/sparsetool.go
+++ sparse/sparsetool/sparsetool.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 
 	"bazil.org/fuse"
 	"bazil.org/fuse/fs"
@@ -16,6 +19,7 @@ var (
 	mountpoint = flag.String("mountpoint", "", "Where to mount")
 	dFile      = flag.String("data", ":memory:", "File with data")
 	oFile      = flag.String("offsets", ":memory:", "File with offsets")
+	bufsize    = flag.Int("bufsize", 0, "Write buffer size for data and offsets")
 	virtFile   = flag.String("virt-file", "111", "Virtual file name")
 	virtSize   = flag.Int64("virt-size", 100e9, "Virtual file size")
 )
@@ -59,15 +63,22 @@ func (a *FileAppender) Size() (int64, error) {
 	return stat.Size(), nil
 }
 
-func open(name string) (sparse.Appender, error) {
+func open(name string) (sparse.Appender, func() error, error) {
 	if name == ":memory:" {
-		return &DummyAppender{}, nil
+		closer := func() error {
+			return nil
+		}
+		return &DummyAppender{}, closer, nil
 	} else {
 		f, err := os.OpenFile(name, os.O_CREATE|os.O_RDWR|os.O_APPEND, 0600)
 		if err != nil {
-			return nil, fmt.Errorf("OpenFile: %v", err)
+			return nil, nil, fmt.Errorf("OpenFile: %v", err)
 		}
-		return &FileAppender{f: f}, nil
+		fa := &FileAppender{f: f}
+		closer := func() error {
+			return f.Close()
+		}
+		return fa, closer, nil
 	}
 }
 
@@ -77,11 +88,11 @@ func main() {
 		flag.PrintDefaults()
 		log.Fatal("Provide -mountpoint")
 	}
-	data, err := open(*dFile)
+	data, dataCloser, err := open(*dFile)
 	if err != nil {
 		log.Fatal(err)
 	}
-	offsets, err := open(*oFile)
+	offsets, offsetsCloser, err := open(*oFile)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -102,6 +113,34 @@ func main() {
 		log.Fatalf("Failed to mount FUSE: %s.", err)
 	}
 	defer mp.Close()
+	var wg sync.WaitGroup
+	// Handle signals.
+	c := make(chan os.Signal, 2)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		sig := <-c
+		fmt.Printf("Caught %s.\n", sig)
+		fmt.Printf("Unmounting %s.\n", *mountpoint)
+		if err := fuse.Unmount(*mountpoint); err != nil {
+			fmt.Printf("Failed to unmount: %s.\n", err)
+		} else {
+			fmt.Printf("Successfully unmount %s.\n", *mountpoint)
+		}
+		fmt.Printf("Closing files.\n")
+		if err := dataCloser(); err != nil {
+			fmt.Printf("Failed to close data: %s.\n", err)
+		} else {
+			fmt.Printf("Successfully closed data.\n")
+		}
+		if err := offsetsCloser(); err != nil {
+			fmt.Printf("Failed to close offsets: %s.\n", err)
+		} else {
+			fmt.Printf("Successfully closed offsets.\n")
+		}
+		fmt.Printf("Exiting.\n")
+	}()
 	err = fs.Serve(mp, f)
 	if err != nil {
 		log.Fatal(err)
@@ -111,4 +150,5 @@ func main() {
 	if err := mp.MountError; err != nil {
 		log.Fatal(err)
 	}
+	wg.Wait()
 }
